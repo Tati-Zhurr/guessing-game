810cc808f4838b6971c6d3905a22d6cebe7ed16b
Tati-Zhurr <zhtv88@gmail.com>
2022-08-05 17:00:17 +0200

    feet: index.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
     constructor() {}
 
+
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+       return this.min, this.max;
 
     }
 
     guess() {
-
+        this.number = (this.min + (Math.ceil((this.max-this.min)/2)))
+    return this.number; 
     }
 
     lower() {
+        this.max = this.number;
+     return this.max;
 
     }
 
     greater() {
-
+        this.min = this.number;
+        return this.min;
     }
 }
 
